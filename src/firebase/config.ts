11435b99e928ccd1f14c198200d75828934e25f2
David Favero <mdfavero@gmail.com>
2025-10-05 19:51:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "997100834225",
+  "appId": "1:997100834225:web:3d8a92b36d9261ab230190",
+  "apiKey": "AIzaSyA__g8yL8EwHsNPW7-E6VON9GsVU6g796o",
+  "authDomain": "localdinkapp.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "997100834225"
+};
